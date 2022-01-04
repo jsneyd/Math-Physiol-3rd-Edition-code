4b6c40c396a8be4a9e736608172556d36dc9a098
jsneyd <sneyd@math.auckland.ac.nz>
2022-01-05 09:51:59 +1300

    chap 19

---
+++ chap19_retina/exercises/visex_rhodop.m
@@ -0,0 +1,22 @@
+%%
+clear all
+close all
+clc
+
+syms k w t x real
+assume(k>0);
+assume(x>0);
+
+n=6;
+Phat = k^n/((k+i*w)^n);
+P = ifourier(Phat);
+pretty(P)
+
+% Just for fun, make sure that the integral of P (from 0 to infinity) is 1.
+% Which it is, of course.
+
+totalint = int(P,0,inf)
+
+% Now calculate the mean of P
+m = int(x*P,0,inf)                  % This comes out to be n/k
+v = int((x-m)^2*P,0,inf)            % This comes out to be n/k^2
