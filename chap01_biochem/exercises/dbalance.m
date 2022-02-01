18b1adafb8bc7568571df07e8bc1c213670daeed
jpkeener <keener@math.utah.edu>
2022-02-01 14:32:51 -0700

    code changes

--- chap01_biochem/exercises/dbalance.m
+++ chap01_biochem/exercises/dbalance.m
@@ -3,9 +3,10 @@ clear all
 close all
 clc
 
-syms a b k2 km1 k1 km2 C0 k3 km3
-fun1 = -(k2 + km1)*a + k1*b + km2*(C0 - a - b) == 0;
-fun2 = -(k1 + km3)*b + km1*a + k3*(C0 - a - b) == 0;
+syms a b k2 K1 k1 K2 C0 k3 K3  
+fun1 = -(k2 + k1*K1)*a + k1*b + k2*K2*(C0 - a - b) == 0;
+fun2 = -(k1 + k3*K3)*b + k1*K1*a + k3*(C0 - a - b) == 0;
 [a,b] = solve([fun1,fun2], [a b]);
 
-simplify(-a*km1 + b*k1)
+V=-a*k1*K1 + b*k1
+[N,D]=numden(V)
