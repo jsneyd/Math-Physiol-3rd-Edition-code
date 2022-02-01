d37129403da8ca5bea9b4cc0b4b5a78efaec28fd
jpkeener <keener@math.utah.edu>
2022-01-31 20:49:07 -0700

    slight changes to code

--- chap01_biochem/exercises/three_sites.m
+++ chap01_biochem/exercises/three_sites.m
@@ -2,7 +2,7 @@ clear all
 close all
 clc
 
-syms c1 c2 c3 k1  k3 k5 e0 s ee K1 K2 K3
+syms c1 c2 c3 k1 k2 k3 k4 k5 k6 e0 s ee K1 K2 K3
 
 ee = e0-c1-c2-c3;
 fun1=k1*s*ee-k1*K1*c1-k3*s*c1+k3*K2*c2==0;
@@ -10,3 +10,7 @@ fun2=k3*s*c1-k3*K2*c2-k5*s*c2+k5*K3*c3==0;
 fun3=k5*s*c2-k5*K3*c3==0;
                 
 [c1,c2,c3]=solve([fun1,fun2,fun3],[c1,c2,c3])
+
+V = k2*c1+k4*c2+k6*c3
+
+[N,D] = numden(V)
