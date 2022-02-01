b06f5b88e7ab9804d493c13362a8dc8fda89ef3c
jsneyd <sneyd@math.auckland.ac.nz>
2022-02-01 13:12:48 +1300

    Update three_sites.m

--- chap01_biochem/exercises/three_sites.m
+++ chap01_biochem/exercises/three_sites.m
@@ -2,11 +2,11 @@ clear all
 close all
 clc
 
-syms c1 c2 c3 k1 km1pk2  k3 k4pkm3 k5 k6pkm5 e0  s  ee
+syms c1 c2 c3 k1  k3 k5 e0 s ee K1 K2 K3
 
-fun1=k1*s*ee-(km1pk2)*c1-k3*s*c1+(k4pkm3)*c2==0;
-fun2=k3*s*c1-(k4pkm3)*c2-k5*s*c2+(k6pkm5)*c3==0;
-fun3=k5*s*c2-(k6pkm5)*c3==0;
-ee=e0-c1-c2-c3;
-                   
+ee = e0-c1-c2-c3;
+fun1=k1*s*ee-k1*K1*c1-k3*s*c1+k3*K2*c2==0;
+fun2=k3*s*c1-k3*K2*c2-k5*s*c2+k5*K3*c3==0;
+fun3=k5*s*c2-k5*K3*c3==0;
+                
 [c1,c2,c3]=solve([fun1,fun2,fun3],[c1,c2,c3])
