ffbb2e58cfa000ef53bbf500fe83c1ecce68bbe1
jsneyd <sneyd@math.auckland.ac.nz>
2022-03-29 12:43:09 +1300

    chap 18

--- chap18_gastrointestinal/exercises/FHN_h_function.py
+++ chap18_gastrointestinal/exercises/FHN_h_function.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
 plt.rcParams['axes.spines.right'] = False  # Me being pedantic and removing the box around the graph
 plt.rcParams['axes.spines.top'] = False
 
@@ -25,7 +26,13 @@ def get_h(phi):
     h = np.trapz(integrand,t_n)/T
     return h
         
-
+def FHN_coupled_deriv(t, y):
+    v1,w1,v2,w2 = y 
+    v1dot = (1/eps)*( v1*(1-v1)*(v1-alpha) - w1) + I + delta*(v2-v1)
+    w1dot = v1 - w1
+    v2dot = (1/eps)*( v2*(1-v2)*(v2-alpha) - w2) + I + delta*(v1-v2)
+    w2dot = v2 - w2
+    return [v1dot,w1dot,v2dot,w2dot]
 
 # First, read in all the data from the output of FHN_adjoint.py. The period,
 # limit cycle, adjoint solution, parameters, etc. So we don't need to recompute
@@ -33,20 +40,38 @@ def get_h(phi):
 
 [alpha,eps,I,T,t_n,v_n,w_n,Va,Wa]= np.load('FHN_save_variables.npy',allow_pickle=True)
 n = len(t_n)
+delta = 0.3   # the coupling strength
 
 sn = 100
 h = np.empty(sn)
-hm = h
 st = np.linspace(0,T,sn)
 for i in range(sn):
     h[i] = get_h(st[i])
-    hm = get_h(-st[i])
-    
-plt.plot(st/T,-h+hm)
-plt.xlabel('time/T')
-plt.ylabel(r'h$(-\phi)-h(\phi)$')
 
+plt.figure(1)
+plt.plot(st/T,h)
+plt.xlabel('$\phi$/T')
+plt.ylabel(r'$h(\phi)$')
 
+plt.figure(2)
+plt.plot(st/T,np.flip(h) - h)
+plt.xlabel('$\phi$/T')
+plt.ylabel(r'$h(-\phi)-h(\phi)$')
 
+# Now solve the coupled oscillator equations directly
+tend = 10*T
+t = np.linspace(0,tend,2000)
+y0 = [0.1,0,-0.3,0.1]
+soln = solve_ivp(FHN_coupled_deriv, (0, tend), y0, method='Radau',t_eval=t,rtol=1e-8,atol=1e-8)
+v1,w1,v2,w2 = soln.y
+
+plt.figure(3)
+plt.plot(soln.t,v1,soln.t,v2)
+plt.xlim([tend-3*T,tend])
 
   
+
+
+
+
+#
