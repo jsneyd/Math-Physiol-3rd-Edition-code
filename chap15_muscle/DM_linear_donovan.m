d5d9648c6944b6fbb23ff0f6bafd2a5896129da5
jsneyd <sneyd@math.auckland.ac.nz>
2022-05-15 19:51:30 +1200

    chap 15

---
+++ chap15_muscle/DM_linear_donovan.m
@@ -0,0 +1,150 @@
+% code to reproduce uniform distribution approximation from Math. Biosci. 239 2020:108444
+% (Generalized distribution-moment approximation for kinetic theories of muscular contraction)
+% GM Donovan
+% g.donovan@auckland.ac.nz
+
+function main
+close all
+clear all
+
+% for constant stretch (Figs 2 & 3), change sign of v as required
+%if(1==0)
+vconst=-10; % +/-
+param.v=@(x)vconst;
+%end
+
+% for oscillations (Fig 4)
+% if(1==1)
+% v0=25;
+% om=50;
+% param.v=@(t) v0*sin(om*t);
+% end
+
+% binding functions
+f1=43.3;
+g1=10;
+g2=209;
+f=@(x) 0 + (x>0 & x<1).*(f1*x);
+g=@(x) g2*(x<=0) + g1*x.*(x>0);
+n0=@(x) f(x)./(f(x)+g(x));
+
+% time domain for integration
+tmax=0.5; % in s
+
+% parameters into a structure so that they're accessible to the sub-functions
+param.h=1;
+param.g1=g1;
+param.g2=g2;
+param.g3=0;
+param.f1=f1;
+
+% moments of f. for piecewise linear f you could of course write them explicitly rather than calculate by quadrature. 
+xv_orig=linspace(-1,3,1000);
+param.b0=trapz(xv_orig,f(xv_orig));
+param.b1=trapz(xv_orig,xv_orig.*f(xv_orig));
+param.b2=trapz(xv_orig,xv_orig.^2.*f(xv_orig));
+
+cb=colormap('lines').';
+
+% moments of (steady-state) IC
+M0_0=trapz(xv_orig,n0(xv_orig));
+M1_0=trapz(xv_orig,xv_orig.*n0(xv_orig));
+M2_0=trapz(xv_orig,(xv_orig).^2.*n0(xv_orig));
+
+% into a vector
+Mbar=[M0_0 M1_0 M2_0];
+
+% integrate
+%[tout,yout]=ode45(@(t,y)DMuni_rhs(t,y,param),[0 tvals(end)],Mbar);
+[tout,yout]=ode45(@(t,y)DMuni_rhs(t,y,param),[0 tmax],Mbar);
+
+% store
+DMuni.t=tout;
+DMuni.y=yout;
+
+% plot
+for qq=1:3
+figure(qq)
+plot(tout,yout(:,qq),'-')
+hold on
+ylabel(sprintf('M_%d',qq-1))
+xlabel('time')
+end
+
+% plot the distributions
+
+% set up the subplots
+N=10;
+sp1=2;
+sp2=5;
+
+figure
+
+ttargs=linspace(0,tmax,N);
+
+for qq=1:N
+        subplot(sp1,sp2,qq)
+
+        [~,thisind]=min(abs(DMuni.t(:)-ttargs(qq)));
+        thisy=DMuni.y(thisind,:);
+        M0=thisy(1);
+        M1=thisy(2);
+        M2=thisy(3);
+        a=M1/M0;
+        b=(sqrt(3)/M0)*sqrt(M0*M2-M1^2);
+        c=(M0^2)/(2*sqrt(3)*sqrt(M0*M2-M1^2));
+
+        plot([a-5*b a-b a-b a+b a+b a+5*b],[0 0 c c 0 0],'color',cb(:,5))
+        ax=axis;
+        ax(1)=a-4*b;
+        ax(2)=a+4*b;
+        ax(3)=-0.1;
+        ax(4)=1.2;
+        axis(ax);
+        title(sprintf('t=%0.2f',ttargs(qq)))
+end
+
+% labels
+subplot(sp1,sp2,1)
+ylabel('n')
+subplot(sp1,sp2,6)
+ylabel('n')
+subplot(sp1,sp2,8)
+xlabel('x')
+subplot(sp1,sp2,10)
+
+end
+
+%% -------------------------------------------------------
+function [rhs] = DMuni_rhs(t,y,param)
+
+rhs=0*y; % get the rightshape
+
+M0=y(1);
+M1=y(2);
+M2=y(3);
+
+% J is implemented in terms of a,b,c instead of M0,M1,M2. (height c between a and b). so get those first
+a=M1/M0;
+b=(sqrt(3)/M0)*sqrt(M0*M2-M1^2);
+c=(M0^2)/(2*sqrt(3)*sqrt(M0*M2-M1^2));
+
+% now the phis
+lam=0;
+phi0=(param.g2*Juni(lam,a,b,c,-Inf,0) + (param.f1+param.g1)*Juni(lam+1,a,b,c,0,1) + (param.g1+param.g3)*Juni(lam+1,a,b,c,1,Inf) - param.g3*Juni(lam,a,b,c,1,Inf)  );
+lam=1;
+phi1=(param.g2*Juni(lam,a,b,c,-Inf,0) + (param.f1+param.g1)*Juni(lam+1,a,b,c,0,1) + (param.g1+param.g3)*Juni(lam+1,a,b,c,1,Inf) - param.g3*Juni(lam,a,b,c,1,Inf)  );
+lam=2;
+phi2=(param.g2*Juni(lam,a,b,c,-Inf,0) + (param.f1+param.g1)*Juni(lam+1,a,b,c,0,1) + (param.g1+param.g3)*Juni(lam+1,a,b,c,1,Inf) - param.g3*Juni(lam,a,b,c,1,Inf)  );
+
+% now the system right-hand side
+rhs(1) = param.b0 - phi0;
+rhs(2) = param.b1 - phi1 - param.v(t)*y(1);
+rhs(3) = param.b2 - phi2 - 2*param.v(t)*y(2);
+end
+
+%% -----------------------------------------------------
+function z=Juni(lam,a,b,c,alph,bet)
+f=@(x) (c/(lam+1))*(x.^(lam+1));
+z= f( max( min(a+b,bet),a-b)) - f(min(max(a-b,alph),a+b));
+end
---
+++ chap15_muscle/dm_linear.py
@@ -0,0 +1,80 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+from scipy.integrate import quad
+import matplotlib.pyplot as plt
+
+
+def deriv(t,y):
+     m0,m1,m2 = y
+     a = m0**2/(2*np.sqrt(3)*np.sqrt(m0*m2-m1*m1)) # value of n where non-zero
+     dum0 = m1/m0                                  
+     dum1 = (np.sqrt(3)/m0)*(np.sqrt(m0*m2-m1*m1))
+     a0 = dum0 - dum1                      #  n is non-zero between b0 and b1
+     a1 = dum0 + dum1  
+     # Now calculate all the limits for the integrals
+     (i1c1,i1c2) = get_limits(0,1,a0,a1)
+     (i2c1,i2c2) = get_limits(-np.inf,0,a0,a1)
+     (i3c1,i3c2) = get_limits(0,np.inf,a0,a1)
+     
+     (int1,int2,int3) = get_integrals(0,a,i1c1,i1c2,i2c1,i2c2,i3c1,i3c2)
+     m0dot = b0 - int1 - int2 -int3
+     int1,int2,int3 = get_integrals(1,a,i1c1,i1c2,i2c1,i2c2,i3c1,i3c2)
+     m1dot = b1 - v(t)*m0 - int1 - int2 -int3
+     int1,int2,int3 = get_integrals(2,a,i1c1,i1c2,i2c1,i2c2,i3c1,i3c2)
+     m2dot = b2 -2*v(t)*m1 - int1 - int2 -int3
+     return [m0dot,m1dot,m2dot]
+
+def get_limits(a1,b1,a2,b2):  # calculates the overlap of two regions
+    c1 = max(a1,a2)
+    c2 = min(b1,b2)
+    if (c2<c1):
+        c1=0
+        c2=0
+    return [c1,c2]
+
+def get_integrals(lam,a,i1c1,i1c2,i2c1,i2c2,i3c1,i3c2):
+     # we have to calculate 3 integrals for each ode. The integral is calculated exactly.    
+     i1 = f1*a/(lam+2)*(i1c2**(lam+2) - i1c1**(lam+2))
+     i2 = g2*a/(lam+1)*(i2c2**(lam+1) - i2c1**(lam+1))
+     i3 = g1*a/(lam+2)*(i3c2**(lam+2) - i3c1**(lam+2))
+     return [i1,i2,i3]
+
+def f(x):
+    return np.piecewise(x,[x<=0,0<=x<=1,x>1],[0,f1*x,0])
+    
+def g(x):
+    return np.piecewise(x,[x<=0,x>0],[g2,g1*x])
+
+def v(t):
+    return v0 + v1*np.sin(50*t)
+
+# ----------------------------------------------------------------
+
+f1=43.3
+g1=10 
+g2=209
+v0 = 0 # +/- 10 for constant velocity solutions
+v1 = 25  # 25 for oscillatory solutions
+
+ff = np.vectorize(f)
+gg = np.vectorize(g)
+
+# This is the range of x for which we do all the integrations
+xp = np.linspace(-1,3,num = 1000)
+b0 = np.trapz(ff(xp),x=xp)
+b1 = np.trapz(xp*ff(xp),x=xp)
+b2 = np.trapz(xp*xp*ff(xp),x=xp)
+
+
+# Initial conditions
+n0 = ff(xp)/(ff(xp)+gg(xp))
+m0_0 = np.trapz(n0,x=xp)
+m1_0 = np.trapz(xp*n0,x=xp)
+m2_0 = np.trapz(xp*xp*n0,x=xp)
+
+tp = np.linspace(0,0.5,100)
+soln1 = solve_ivp(deriv,[0, 0.5],[m0_0,m1_0,m2_0],method='Radau',t_eval=tp)
+plt.plot(soln1.t,soln1.y[0],soln1.t,soln1.y[1],soln1.t,soln1.y[2])
+
+#np.savetxt('moments',np.transpose(soln1.y))
+#np.savetxt('moments_time',np.transpose(soln1.t))
