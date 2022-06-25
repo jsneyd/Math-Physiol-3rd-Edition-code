45b7362449c4b26c1ab1cecf7dc449a64786e243
jsneyd <sneyd@math.auckland.ac.nz>
2022-06-26 09:14:29 +1200

    chap 5

--- chap05_excitability/hh_fast_slow.m
+++ chap05_excitability/hh_fast_slow.m
@@ -1,16 +1,27 @@
 
-function main
+function hh_fast_slow
+clear all
+close all
+clc
 
 global Iapp gna gk gl vna vk vl cm
 global dumv
 
-Iapp=50.0;
+Iapp=0.0;
 gna=120.0; gk=36.0; gl=0.3;
 vna=115.0; vk=-12.0; vl=10.6; cm=1.0;
 
+plot_nullclines
+
+end
+
+%%
+function plot_nullclines
+global Iapp gna gk gl vna vk vl cm
+global dumv
 n0=0;
-for i=1:200
-    dumv=-12+127*i/200;
+for i=1:500
+    dumv=-12+127*i/500;
     v(i)=dumv;
     n(i)=fminsearch(@nullcline,n0);
     n0=n(i);
@@ -19,11 +30,11 @@ for i=1:200
     nbar(i)=alphan/(alphan+betan);
 end
 plot(v,n,v,nbar)
-output=[v' n' nbar'];
+%output=[v' n' nbar'];
+%save fastslow.dat output -ascii
+end
 
-save fastslow.dat output -ascii
-
-%%%%%%%%%%%%%%%%%%%%%%%%
+%%
 function out=nullcline(n)
 global Iapp gna gk gl vna vk vl cm
 global dumv
@@ -33,3 +44,4 @@ betam=4.0*exp(-(dumv)/18.0);
 m=alpham/(alpham + betam);
 
 out=((n^4)*gk*(dumv-vk) + gna*(m^3)*(0.8-n)*(dumv-vna) + gl*(dumv-vl) - Iapp)^2;
+end
