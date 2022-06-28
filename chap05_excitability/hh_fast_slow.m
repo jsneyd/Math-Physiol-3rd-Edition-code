0129caf748abdc030804891a5a5205de79a5dc4b
jsneyd <sneyd@math.auckland.ac.nz>
2022-06-29 06:15:41 +1200

    chap 5

--- chap05_excitability/hh_fast_slow.m
+++ chap05_excitability/hh_fast_slow.m
@@ -7,7 +7,7 @@ clc
 global Iapp gna gk gl vna vk vl cm
 global dumv
 
-Iapp=0.0;
+Iapp=40.0;
 gna=120.0; gk=36.0; gl=0.3;
 vna=115.0; vk=-12.0; vl=10.6; cm=1.0;
 
@@ -26,12 +26,12 @@ for i=1:500
     n(i)=fminsearch(@nullcline,n0);
     n0=n(i);
     alphan=0.01*(10.0-dumv)./(exp((10.0-dumv)/10.0)-1.0);
-    betan=0.125*exp(-(dumv)/80.0);
+    betan=0.125*exp(-(dumv)/19.7);
     nbar(i)=alphan/(alphan+betan);
 end
 plot(v,n,v,nbar)
-%output=[v' n' nbar'];
-%save fastslow.dat output -ascii
+output=[v' n' nbar'];
+save fastslow.dat output -ascii
 end
 
 %%
