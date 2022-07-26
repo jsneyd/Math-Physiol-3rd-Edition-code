5004474f8f1be911283eb7d059d248ea3fdb5dfa
jsneyd <sneyd@math.auckland.ac.nz>
2022-07-27 06:33:48 +1200

    chap 16

--- chap16_endocrine/sturis_one_insulin_pool.m
+++ chap16_endocrine/sturis_one_insulin_pool.m
@@ -3,11 +3,12 @@ clear all; close all; clc;
 
 global input Vp Vi Vg E tp ti td Rm a1 C1 C2 C3 C4 C5 Ub U0 Um Rg alpha beta
 
-input = 216;
-Vp=3; Vi=11; Vg=10; E=0.2; tp=6; ti=100; td=12; Rm=209; a1=6.6; C1=300;
+input = 100;
+td=12;
+Vp=3; Vi=11; Vg=10; E=0.2; tp=6; ti=100;  Rm=209; a1=6.6; C1=300;
 C2=144; C3=100; C4=80; C5=25.86; Ub=72; U0=4; Um=94; Rg=180; alpha=7.5; beta=1.77;
 
-initial = [1 1 1 1 1 1];
+initial = [1 1 1 1 1 ];
 tspan = linspace(0,2000,1000);
 
 [tt,sol]=ode45(@sturisfun,tspan,initial);
@@ -17,18 +18,17 @@ plot(tt,sol(:,1)/Vp)
 figure(2)
 plot(tt,sol(:,3)/(10*Vg))  % Output per dL, as in Sturis
 
-save sturis.dat sol -ASCII
+%save sturis.dat sol -ASCII
 
 %%
 function dydt=sturisfun(t,y)
 global Vp Vi E tp ti td input
 
-Ip=y(1); Ii=y(2); G=y(3); h1=y(4); h2=y(5); h3=y(6); 
+I=y(1); G=y(2); h1=y(3); h2=y(4); h3=y(5); 
 
-dydt = [eff1(G) - (Ip/Vp-Ii/Vi)*E - Ip/tp
-        (Ip/Vp-Ii/Vi)*E - Ii/ti
-        eff5(h3) + input - eff2(G) - eff3(G)*eff4(Ii)
-        (Ip - h1)/td
+dydt = [eff1(G) - I/tp
+        eff5(h3) + input - eff2(G) - eff3(G)*eff4(I)
+        (I - h1)/td
         (h1-h2)/td
         (h2-h3)/td
         ];
