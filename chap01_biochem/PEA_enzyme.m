7dc6b151f25a22b6a12b8970fbb5d7437f411add
jpkeener <keener@math.utah.edu>
2022-12-24 11:10:06 -0700

    codes changes

--- chap01_biochem/PEA_enzyme.m
+++ chap01_biochem/PEA_enzyme.m
@@ -1,5 +1,5 @@
 function enzyme
-% asymptotic analysis of enzyme kinetics equations
+% asymptotic analysis of enzyme kinetics equations - PEA
 clear all
 close all
 clc
@@ -43,38 +43,7 @@ save('temp1.dat','forplotting1')
 forplotting2 = [slow_s ; slow_z;  slow_x]';
 save('temp2.dat','forplotting2')
 
-
-% Part 2:  QSS approximation
-par.a = 0.5;
-par.k = 1.5;
-par.e = 0.05;
-
-tspan = linspace(0,20,2000);
-initial = [1,0];
-[t,Y] = ode15s(@(t,y)rhsqss(t,y,par),tspan,initial);
-s=Y(:,1);
-x=Y(:,2);
  
-figure(4)
-plot(t,s,t,x,'linewidth',2)
-legend('\sigma','z','fontsize',18)
-xlabel('\eta','fontsize',20)
-axis([0 4 0 1])
-% now plot the slow manifold and the solution together
-slow_s = linspace(0,1.2,100);
-slow_x = slow_s./(slow_s+par.k) ;
-figure(5)
-plot(s,x,slow_s,slow_x,'--','linewidth',2)
-xlabel('\sigma','fontsize',20)
-ylabel('x','fontsize',20)
-axis([0 1.2 0 0.5])
- 
-forplotting3 = [t s x];
-save('temp3.dat','forplotting3')
-forplotting4 = [slow_s ;  slow_x]';
-save('temp4.dat','forplotting4')
-
-
 end
 
 
@@ -85,12 +54,4 @@ out(1) = z-s - par.b*s*(par.a + s - z);
 out(2) = par.e*(s-z);
 out = out';  % don't forget this or you get rubbish.
 end
-
-function out = rhsqss(t,y,par)
-s = y(1);
-x = y(2);
-out(1) = -s+x*(s+par.a) ;
-out(2) = (s-x*(s+par.k))/par.e;
-out = out';  % don't forget this or you get rubbish.
-end
-
+ 
---
+++ chap01_biochem/QSSA_enzyme.m
@@ -0,0 +1,53 @@
+function enzyme
+% asymptotic analysis of enzyme kinetics equations - QSSA
+clear all
+close all
+clc
+set(0,                           ...
+   'defaultaxesfontsize', 20,   ...
+   'defaultaxeslinewidth', 1.0, ...
+   'defaultlinelinewidth', 1.2, ...
+   'defaultpatchlinewidth', 0.7);
+% Part 2:  QSS approximation
+par.a = 0.5;
+par.k = 1.5;
+par.e = 0.05;
+
+tspan = linspace(0,20,2000);
+initial = [1,0];
+[t,Y] = ode15s(@(t,y)rhsqss(t,y,par),tspan,initial);
+s=Y(:,1);
+x=Y(:,2);
+ 
+figure(4)
+plot(t,s,t,x,'linewidth',2)
+legend('\sigma','z','fontsize',18)
+xlabel('\eta','fontsize',20)
+axis([0 4 0 1])
+% now plot the slow manifold and the solution together
+slow_s = linspace(0,1.2,100);
+slow_x = slow_s./(slow_s+par.k) ;
+figure(5)
+plot(s,x,slow_s,slow_x,'--','linewidth',2)
+xlabel('\sigma','fontsize',20)
+ylabel('x','fontsize',20)
+axis([0 1.2 0 0.5])
+ 
+forplotting3 = [t s x];
+save('temp3.dat','forplotting3')
+forplotting4 = [slow_s ;  slow_x]';
+save('temp4.dat','forplotting4')
+
+
+end
+
+ 
+
+function out = rhsqss(t,y,par)
+s = y(1);
+x = y(2);
+out(1) = -s+x*(s+par.a) ;
+out(2) = (s-x*(s+par.k))/par.e;
+out = out';  % don't forget this or you get rubbish.
+end
+
