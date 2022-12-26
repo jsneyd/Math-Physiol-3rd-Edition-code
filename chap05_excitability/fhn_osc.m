46fffe11d06d36b3d2164ac6e88dc34909165f06
jsneyd <sneyd@math.auckland.ac.nz>
2022-12-27 06:50:39 +1300

    Create fhn_osc.m

---
+++ chap05_excitability/fhn_osc.m
@@ -0,0 +1,44 @@
+% Compute an oscillation in the FHN
+% equations.
+
+% For Keener and Sneyd, Chapter 5, Figure ??
+
+close all
+clear all
+clc
+set(0,                           ...
+   'defaultaxesfontsize', 14,   ...
+   'defaultaxeslinewidth', 1.2, ...
+   'defaultlinelinewidth', 2.0); 
+
+global p
+p.eps = 0.0001; p.alpha = 0.1; p.gamma = 0.5;
+p.Iapp = 0.5;
+
+tend = 4; nt = 50000;
+init = [0.2,0];
+tspan = linspace(0,tend,nt);
+[t,sol] = ode15s(@(t,x)fhnrhs(t,x),tspan,init);
+
+figure(1)
+plot(t,sol(:,1),'r')  % time series
+
+
+figure(2)  % phase plane
+v = linspace(-0.4,1.4,100);
+w1 = v.*(p.alpha-v).*(v-1) + p.Iapp;
+w2 = v/p.gamma;
+plot(sol(:,1),sol(:,2),'r',v,w1,'g--',v,w2,'b--')
+ylim([0.4,0.7]);
+xlabel('v'); ylabel('w');
+
+
+% ----------------
+function out=fhnrhs(t,x)
+global p
+v=x(1);
+w=x(2);
+out(1) = (v.*(p.alpha-v).*(v-1) - w + p.Iapp)/p.eps;
+out(2) = v-p.gamma*w;
+out = out';
+end
