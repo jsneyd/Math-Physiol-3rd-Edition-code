04e153bb1542c531012ce54ed063e25abadcc5bc
jpkeener <keener@math.utah.edu>
2023-08-18 21:16:02 -0600

    Update facilitation.m

--- chap08_intercellular/facilitation.m
+++ chap08_intercellular/facilitation.m
@@ -1,11 +1,14 @@
 % facilitation curve
-
+ set(0,                           ...
+   'defaultaxesfontsize', 20,   ...
+   'defaultaxeslinewidth', 2.0, ...
+   'defaultlinelinewidth', 2.0);
 tpcp=200;
 k1=3.75e-3;
 k2=2.5e-3;
 k3=5e-4;
 
-km1=4e-4;
+km1=4e-4;  %time units are ms
 km2=1e-3;
 km3=0.1;
 K1=km1/k1;
@@ -20,7 +23,7 @@ a3=exp(-km3*(T+tpcp/K3));
 Fmax = (1./(1-a1)).*(1./(1-a2)).*(1./(1-a3)); 
     
 
-semilogx(1000./T,Fmax)
+semilogx(1000./T,Fmax)% the factor 1000 is to convert to Hertz  = s^{-1}
 xlabel('Stimulus Frequency (Hz)')
 ylabel('Facilitation')
 axis([0.1 100 0 7])
