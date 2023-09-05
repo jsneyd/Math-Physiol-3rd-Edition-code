ecbe960925f1513593103c57d8dbf2cb8669816f
jpkeener <keener@math.utah.edu>
2023-09-04 19:08:40 -0600

    edits

--- chap08_intercellular/full_synaptic_cleft_model.m
+++ chap08_intercellular/full_synaptic_cleft_model.m
@@ -10,15 +10,18 @@ set(0,                           ...
    'defaultlinelinewidth', 2.0); 
 %parameters
 global Vr k10  k20  z1  n ce PCa s0 
-global FbyRT  F  kout    
+global FbyRT  F  kout  factor  
 global   kp1  km1 kp2  km2  kp3  km3 kp4   km4 
 global   bigA  bigB   bigN smalla  smallb k1m  k2m  ke 
 global Vs  gr  gs0 Cm Istim Tperiod  rho
 
+tmax = 15;  %this is the length of the simulation
 % FHN parameters
-Vr=-70;
+Vr=-70;  % resting potential 
+Tperiod =tmax;  % period of stimulus
+ Istim=100; % stimulus amplitude
 
-% Llinas parameters
+ % Llinas parameters
  k10=2;
  k20=1;
  z1=1;
@@ -29,21 +32,23 @@ Vr=-70;
  s0=20;
 
 %Voltage-type parameters
-F=96490; R=8.315; Tp=300;
+F=96490; % Faraday's constant
+R=8.315;  % Gas constant
+Tp=300; % Temperature in degrees Kelvin
  FbyRT = F/(R*Tp*1000); % units of mV^(-1)
- 
-
+  
 % Calcium parameters. Radius in microns. Factor converts current to calcium flux.
   
 factor=(3/20)*(1/(2*F));
+kout = 3; % calcium withdrawal rate
 
-% Calcium-stimulated secretion parameters
+% Calcium-stimulated secretion parameters for o_j dynamics
  kp1=3.75e-3;
- km1=4e-4;  % this is very slow
- %km1=4e-3;
+ km1=4e-4;   
+ %km1=1e-4;%these rates are extremely slow.   
  kp2=2.5e-3; 
- km2=1e-3;  % this is very slow
- %km2 = 1e-2;
+ km2=1e-3;   
+ %km2 = 5e-2; %these rates are extremely slow.  
  kp3=5e-4;
  km3=0.1;
  kp4=7.5e-3;
@@ -53,29 +58,29 @@ factor=(3/20)*(1/(2*F));
 % Magleby parameters
  bigA=0.008;
  bigB=1.43;
- bigN=10;
+ bigN=10;  %dimensionless
 smalla=0.00315; 
 smallb=1;
- k1m=1000; %ms^(-1)mM^(-1) 
- k2m=500; %ms^(-1)
+ k1m=1000; %ms^(-1) 
+ k2m=500; %mM ms^(-1)
  ke=10;% rate of ACh degradation
 
 % Postsynaptic voltage parameters
- Vs=-15;
+ Vs=-15; 
  gr=10;
  gs0 = 10;
  Cm=1;
 
 
 % there are twelve equations
- Istim=3;
- Tperiod = 30;
 
-tt=[0:.01:10];
-init=[-50, 0,0.11,1.4e-6,0.2,0.06,0.0015,0,0,0,0,-70];
+tt=[0:.01:tmax];
+init=[-70, 0,0.11,1.4e-6,0.14,0.05,0.0015,0,0,0,0,-70];
 [T,sol] = ode15s(@(t,S)rhs(t,S),tt,init);
+
+
 v1=sol(:,1);
-v2=sol(:,12);
+w = sol(:,2);
 op=sol(:,3);
 c = sol(:,4);
 o1=sol(:,5);
@@ -85,23 +90,16 @@ o4=sol(:,8);
 Pr = o1.*o2.*o3.*o4;
 
 x = sol(:,9);
-o1(end)
-o2(end)
-o3(end)
-o4(end)
-
 
 y=sol(:,10); 
 a = sol(:,11);
+v2=sol(:,12);
 
-figure(1)
-
+ figure(1)
  plot(T,v1)
- 
 xlabel('t (ms)')
-ylabel('V_1')
-
-
+ylabel('V_1 (ms)')
+ 
 figure(2)
 plot( T,op)
 ylabel('o')
@@ -111,34 +109,43 @@ figure(3)
 plot(T,c)
  ylabel('c (\muM)')
 xlabel('t (ms)')
-
+ 
 figure(4)
 plot(T,o1,T,o2,T,o3,T,o4 )
+legend('boxoff')
 legend('o_1','o_2','o_3','o_4')
+xlabel('t (ms)')
 
 figure(5)
 plot(T,Pr)
 ylabel('P_R') 
-
-figure(6)
+xlabel('t (ms)')
  
- plot(T,a,T,x,T,y)
- legend('a','x','y')
+figure(6)
+ plot(T,a )
  xlabel('t (ms)')
+ylabel('a (mM)')
 
- figure(7)
- plot(T,v1,T,v2)
- legend('V_1,','V_2')
-  
-
+figure(7)
+ plot(T,x,T,y,'--')
+ legend('boxoff')
+ legend('x','y')
+ xlabel('t (mV)')
+ 
+ figure(8)
+ plot(T,v1,T,v2,'--')
+ legend('boxoff')
+ legend('presynaptic,','postsynaptic')   
+xlabel('t (mV)')
+ylabel('Membrane Potential (ms)')
 
 %%
 function out = rhs(t,sol)
 global Vr k10  k20  z1  n ce PCa s0 
-global FbyRT  F  kout 
+global FbyRT  F  kout  factor
 global   kp1  km1 kp2  km2  kp3  km3 kp4   km4 
 global   bigA  bigB   bigN smalla  smallb k1m  k2m  ke 
-global Vs  gr  Cm gs0 rho
+global Vs  gr  Cm gs0 rho Tperiod Istim
 
 %there are 12 variables
 v1=sol(1); %presynaptic potential
@@ -184,18 +191,19 @@ beta=smallb*exp(smalla*v2);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
  
 % FHN odes for the presynaptic voltage
-v1p=100*(0.0001*(v1-Vr)*(70-(v1-Vr))*((v1-Vr)-7)-w);
+ 
+s=t-fix(t/Tperiod)*Tperiod -1;
+
+Iapp = Istim*1./cosh(5*s);
+
+v1p=100*(0.0001*(v1-Vr)*(70-(v1-Vr))*((v1-Vr)-7)-w) +Iapp;
 wp =0.25*(v1-Vr-5*w);
 
 %Llinas odes
 op=k1*(1-o) - k20*o;
 
 %Calcium odes. Conversion factors are included explicitly, for clarity
-
- kout=2;
  
-factor=(3/20)*(1/(2*F));
-
 cp= -ICa*factor - kout*c;
 
 %Calcium-stimulated secretion odes
