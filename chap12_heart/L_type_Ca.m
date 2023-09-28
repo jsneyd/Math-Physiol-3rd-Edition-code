855e41ccae71ba57c7ded05cacc88acf02c9b2b1
jpkeener <keener@math.utah.edu>
2023-09-28 11:07:17 -0600

    code edits

--- chap12_heart/L_type_Ca.m
+++ chap12_heart/L_type_Ca.m
@@ -9,8 +9,7 @@ set(0,                           ...
    'defaultaxesfontsize', 18,   ...
    'defaultaxeslinewidth', 2.0, ...
    'defaultlinelinewidth', 2.0); 
-
-
+ 
 %parameters
 Vm = -75;
 c = 0;
@@ -32,28 +31,20 @@ tend=15;
 init = [0.965,0.0248,0,0,0.0102,0];
 tspan = [0:dt:tend];
 [t,sol] = ode15s(@(t,x)Ltrhs(t,x,p),tspan,init);
-
-
+ 
   op=sol(:,5);
   c = sol(:,6)
   figure(1)
 
   plot(t,op)
   hold on
-
-
+ 
   figure(2)
   plot(t,c)
   hold on
 
  end
-
  
-
-
-    
-    
-    
 function out=Ltrhs(t,x,p)
  
 n3=x(1);
@@ -70,8 +61,7 @@ c3p=-(2*p.alpha+p.omega)*c3+p.gamma*n3+p.beta/2*c4;
 c4p=-(p.beta/2+p.omega)*c4+2*p.gamma*n4+2*p.alpha*c3;
 opp = -p.g*op+p.f*n4;
 cp = op-c;
-
-
+ 
 out=[n3p;n4p;c3p;c4p;opp;cp];
 
  
--- chap12_heart/L_type_Ca_3state.m
+++ chap12_heart/L_type_Ca_3state.m
@@ -18,7 +18,7 @@ p.omega=0.02;
 p.g=2;
 
  
- Vmlist=[-75,  -55, -35, -15, 25,45,65];
+ Vmlist=[-75,  -55, -35, -15, 5,25,45];
  for j=1:length(Vmlist)
      Vm=Vmlist(j);
 
@@ -44,19 +44,22 @@ tspan = [0:dt:tend];
   figure(1)
   plot(t,op )
   hold on
-
+xlabel('time (ms)')
+ylabel('Open Probability')
+    
  
  end
+ legend('boxoff')
 
- 
+ legend('V=-75','V=-55','V=-35','V=-15','V=5','V=25','V=45')
+ set(gca,'linewidth',1.5)
+box off
+hold off
 
-
-    
-    
-    
+  
 function out=Ltrhs(t,x,p)
 
-c=(t>3)*10;
+c=(t>3)*2;
 p.gamma = 0.44*c;
 p.gammabar = p.gamma*(p.beta+2*p.alpha)/(p.alpha+p.beta);
 op=x(1);
