562a34c8d54e062854b20dc14b1ae102b6866e5e
jpkeener <keener@math.utah.edu>
2023-10-31 12:50:09 -0600

    codes

--- chap12_heart/pl_fhn_waves.m
+++ chap12_heart/pl_fhn_waves.m
@@ -1,4 +1,4 @@
-% ectopic focus oscillations
+% using piecewise linear FHN
 % here we use the method of lines to compute waves
 function ectopic_waves
 
@@ -7,30 +7,30 @@ set(0,                           ...
    'defaultaxesfontsize', 20,   ...
    'defaultaxeslinewidth', 1.5, ...
    'defaultlinelinewidth', 2.0)
-global alps gam Dscal A x N eps
+global alf gam Dscal A x N eps dx Ic 
 % parameters
 
 eps = 0.1;
 gam = 0.1;
-alf =0.;
-b = 0.5;
-scale = 1.5;
-N=200; % number of interior grid points
-L = 20; % length of the domain 
+ 
+alf =0.15;
+Ic = 150; % current input  
+ 
+N=100; % number of interior grid points
+L = 10; % length of the domain 
 
 dx = L/(N);
 x=[1:N]'*dx;
-alps=alf +(b-alf)*(exp(-(x/scale).^2));  % spatially inhomogeneous alpha
-  
- Dl=1;
-Dscal = Dl/dx^2;
-% impose a no-flux Neumann boundary condition at both ends
-A = -2*diag(ones(N,1)) + diag([2;ones(N-2,1)],1) + diag([ ones(N-2,1);2],-1);
  
-V0=zeros(N,1);
+ Dl=1;  %diffusion coefficient
+Dscal = Dl/dx^2;
+% impose a  Neumann boundary condition at both ends
+A = -2*diag(ones(N,1)) + diag([2;ones(N-2,1)],1) + diag([ ones(N-2,1);2],-1);
+ %initial data
+V0=ones(N,1)*alf/(1+gam);;
 W0 = zeros(N,1);
 tstep = 0.5;
-t_end = 150;
+t_end = 200;
 
 tspan = [0:tstep:t_end];
 
@@ -38,32 +38,33 @@ s0 = [V0;W0 ];
 
 [T,S] = ode23(@deRHS,tspan, s0 );  
 [t,X]=meshgrid(T,x);
- 
+ figure(1)
 mesh(X,t,S(:,1:N)')
 xlabel('x')
 ylabel('t')
 zlabel('v')
-
-
-   
+ 
+figure(2)
+plot(x,S(end,1:N),x,0.25*ones(1,N),'--')
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %the right hand side for MOL ode simulation:
 function s_prime=deRHS(t,u)
-global Dscal   A N gam  alps eps
+global Dscal   A N gam  alf eps Ic dx
  
 v=u(1:N);
 w=u(N+1:2*N);
  
  vp= Dscal*A*v +  f(v)-w;
- wp=eps*(v-gam*w-alps);
+ vp(1) = vp(1) +2*Ic/dx;
+ wp=eps*(v-gam*w-alf);
  s_prime =[vp;wp];
 end 
  
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function out = f(u)
  
-out = 10*u.*(1-u).*(u-0.5)  ;
+out = -u.*(u<=1/4) + (u>1/4&u<3/4).*(u-1/2) + (u>=3/4).*(1-u);
 end
 
  
--- chap12_heart/pl_schrodinger.m
+++ chap12_heart/pl_schrodinger.m
@@ -1,24 +1,74 @@
 %pl schrodinger equation
+ set(0,                           ...
+   'defaultaxesfontsize', 20,   ...
+   'defaultaxeslinewidth', 1.2, ...
+   'defaultlinelinewidth', 2.0, ...
+   'defaultpatchlinewidth', 0.7); 
+
+ 
 global x1 x2
-mu =[-1:.01:1];
+mu =[-1:.01:1];  %mu is the proposed eigenvalue
+ 
+
 lam =sqrt(1-mu);
 eta=sqrt(1+mu);
-x1=1
-x2=0.75
-
-
-  %x1 = atan(eta./lam)./lam;
+  x1 = atan(eta./lam)./lam;
  
 figure(1)
-plot(mu,f(mu))
+plot(x1,mu)
 
+  stop
+ x1=1
+x2=0.75
+
+% guess a lower bound
+a = -1;
+
+% guess an upper bound
+b = 0.99;
+
+%this is the answer - call the function bisect(a,b,@fval)
+
+r = bisect(a,b,@fval)
+
+figure(1)
+plot(mu,fval(mu), r,fval(r),'*')
+
+
+function root = bisect(a,b,feval)
+
+ul = a;
+fl = feval(ul);
+uu = b;
+fu = feval(uu);
+
+% we make the assumption, without checking, that 
+% fu*fl<0
+
+% if not, the algoritm fails to find a root.
+
+N = 20;  % number of iterates
+% the main bisection algorithm
+for j = 1:N
+uc = (ul+uu)/2;
+fc = feval(uc);
+ftest = (fc.*fl>0);
+ul = ftest.*uc+(1-ftest).*ul;
+fl = ftest.*fc + (1-ftest).*fl;
+
+uu = (1-ftest).*uc+ ftest.*uu;
+fu = (1-ftest).*fc + ftest.*fu;
+end
+root = uc;
+
+end
 %given x1 and x2 find mu by bisection
-function out=f(mu);
+function f=fval(mu);
 global x1 x2
 lam =sqrt(1-mu);
 eta=sqrt(1+mu);
 E=exp(eta*x2);
-  out=E.*(cos(lam*x2) .*sin(lam*x1).*(eta.^2-lam.^2) ...
+  f=E.*(cos(lam*x2) .*sin(lam*x1).*(eta.^2-lam.^2) ...
       + 2*cos(lam*x2) .*cos(lam*x1).*eta.*lam ...
       + sin(lam*x2) .*cos(lam*x1).*(lam.^2-eta.^2)) ...
       + (sin(lam*x2) .*cos(lam*x1).*(eta.^2+lam.^2) ...
