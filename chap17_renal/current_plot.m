a46a333cdaffaf80b57ad4a7ba7b9943a4f82607
jpkeener <65625233+jpkeener@users.noreply.github.com>
2024-01-18 17:24:40 -0700

    new code

---
+++ chap17_renal/current_plot.m
@@ -0,0 +1,21 @@
+% co-current/countercurrent comparison
+clear all; close all; clc;
+set(0,                           ...
+   'defaultaxesfontsize', 20,   ...
+   'defaultaxeslinewidth', 1.2, ...
+   'defaultlinelinewidth', 2.0); 
+
+
+alpha = 10;
+x = [0:.01:1];
+
+Cco = (1-exp(-2*alpha*x))/2;
+Ccount = alpha*x/(1+alpha);
+
+figure(1)
+plot(x,Cco,x,Ccount)
+xlabel('x/L')
+ylabel('C_1/C')
+text(.2,.55,'cocurrent','fontsize',18)
+text(.55,.8,'countercurrent','fontsize',18)
+
