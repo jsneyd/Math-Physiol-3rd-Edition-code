a77942d37273b80289bad2153fecfafde4dfda8c
jpkeener <65625233+jpkeener@users.noreply.github.com>
2024-04-25 15:22:12 -0600

    Update gold_kosh.m

--- chap01_biochem/gold_kosh.m
+++ chap01_biochem/gold_kosh.m
@@ -20,20 +20,34 @@ set(0,                           ...
    'defaultpatchlinewidth', 0.7);
 
 w = linspace(0,1,100);
-K1 = 0.1; K2 = 0.05;
+
+K1vals = [0.1,1.1,0.1];
+K2vals = [0.05,1.2,1.2];
+ 
+K1=0.1;
+K2 = 0.05;
 vrat = (1-w).*(K1+w)./((w.*(K2+1-w)));
 plot(vrat,w,'r','LineWidth',2)
 hold on
 xlim([0,4])
+ 
+K1=1.1;
+K2 = 1.2;
 
-K1 = 1.1; K2 = 1.2;
 vrat = (1-w).*(K1+w)./((w.*(K2+1-w)));
-plot(vrat,w,'--b','LineWidth',2)
+plot(vrat,w,'b--','LineWidth',2)
+hold on
+xlim([0,4])
+ 
+K1=0.1;
+K2 = 1.2;
 
-K1 = 0.1; K2 = 1.2;
 vrat = (1-w).*(K1+w)./((w.*(K2+1-w)));
-figure(1)
-plot(vrat,w,'--g','LineWidth',2)
-box off
+plot(vrat,w,'g--','LineWidth',2)
+hold on
+xlim([0,4])
+ 
+  legend('K_1=0.1, K_2=0.05','K_1=1.1, K_2=1.2','K_1=0.1, K_2=1.2')
+ box off
 xlabel('v_1/v_2')
 ylabel('w')
