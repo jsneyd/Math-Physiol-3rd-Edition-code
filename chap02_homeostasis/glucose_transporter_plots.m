78a1bcd68c99376fc2deb483ed52d0c36485b63b
jpkeener <65625233+jpkeener@users.noreply.github.com>
2024-04-26 19:31:25 -0600

    Update glucose_transporter_plots.m

--- chap02_homeostasis/glucose_transporter_plots.m
+++ chap02_homeostasis/glucose_transporter_plots.m
@@ -17,11 +17,11 @@ set(0,                           ...
 k = 0.5;
  se=[0:.1:10];
 si=0;
-J0 = flux(se,si,k)
+J0 = flux(se,si,k);
 si=1;
-J1 = flux(se,si,k)
+J1 = flux(se,si,k);
 si=2;
-J2 = flux(se,si,k)
+J2 = flux(se,si,k);
 figure(1)
 plot(se,J0,'g',se,J1,'r',se,J2,'b',se,zeros(length(se),1),'--','linewidth',2)
 legend('\sigma_i=0','\sigma_i=1','\sigma_i=2','fontsize',18)
