822943429698f90122f88265aa9bf4f5553ec85a
jpkeener <65625233+jpkeener@users.noreply.github.com>
2024-05-04 13:57:38 -0600

    Update stochastic_gluc_Gill.m

--- chap02_homeostasis/stochastic_gluc_Gill.m
+++ chap02_homeostasis/stochastic_gluc_Gill.m
@@ -1,6 +1,7 @@
 %-------------------------------------------------------------------
 
-% Matlab code to compute the flux through a stochastic model of a glucose transporter, using the Gillespie method.
+% Matlab code to compute the flux through a stochastic model of a glucose transporter, 
+% using the Gillespie algorithm.
 
 % For Chapter 2,  of
 % Keener and Sneyd, Mathematical Physiology, 3rd Edition, Springer.
@@ -25,17 +26,17 @@ ci = 0.1; ce = 10; % these concentrations are held fixed
 
  %states are 
  % S=1, Ce
- %S=2, Pe
- %S=3, Pi
- %S=4, Ci
+ % S=2, Pe
+ % S=3, Pi
+ % S=4, Ci
 
 %the notation here is 
 %G_{i,j} = reaction rate for going from state j to state i  
 
-G = [0 km 0 k;
-    ce*kp 0 k 0;
-    0 k 0 ci*kp;
-    k 0 km 0]
+G = [0    km 0 k;
+    ce*kp 0  k 0;
+    0     k  0 ci*kp;
+    k     0 km 0];
 CG = cumsum(G)
 GS = CG(4,:);
  
@@ -46,14 +47,14 @@ S = zeros(1,num); S(1) = 1;  % S can be 1, 2, 3, or 4
 T = zeros(1,num);
 count= zeros(4,4); % this is where we count the number of reactions  
 trans = zeros(1,num); 
-% During the simulation count the number of glucose molecules    transferred in each direction.
+% During the simulation count the number of glucose molecules transferred in each direction.
 for i = 1:num-1
         r1 = rand;
         r2 = rand;  % Note that you need two random numbers. One for the time, and one for the choice of state.
         K = GS(S(i));
         T(i+1) = T(i) - (1/K)*log(r1);
         S(i+1) = find(CG(:,S(i))>r2*GS(S(i)),1);  %This determines the next state
-   count(S(i+1),S(i)) =  count(S(i+1),S(i)) +1;
+   count(S(i+1),S(i)) =  count(S(i+1),S(i)) +1;  % this counts which reaction occurs
   trans(i+1) = (count(2,1)-count(1,2)) ;
 end
  
