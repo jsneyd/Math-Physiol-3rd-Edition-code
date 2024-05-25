cab7dbf87da61200d8f49f977a40e7a630f5a2d3
James Sneyd <sneyd@math.auckland.ac.nz>
2024-05-26 08:59:07 +1200

    chap 7

--- chap07_calcium/discrete_cal.m
+++ chap07_calcium/discrete_cal.m
@@ -1,10 +1,23 @@
-%this file makes some plots for the discrete release standing waves
 
-%two parameters
+%-------------------------------------------------------------------
+%
+% Discrete release standing waves.
+%
+% For Chapter 7, Section 7.8 of
+% Keener and Sneyd, Mathematical Physiology, 3rd Edition, Springer.
+%
+% Written by James Keener and James Sneyd
+%
+%-------------------------------------------------------------------
+
+clear all
+close all
+clc
+
 Abyk =5;
 beta = [0.01:.1:5];
 
-astar = 1-sqrt(1/(1+Abyk))
+astar = 1-sqrt(1/(1+Abyk));
 Af = Abyk.*beta.*sinh(beta);
 l0 = cosh(beta);
 lf = l0+Af/2;
@@ -16,14 +29,13 @@ C = Af./(Af+2*l0-2);
 a = C.*(muf-1)./(muf-1./mu0);
 b = C.*(1-1./mu0)./(muf-1./mu0);
 bf = C-b.*muf;
+
 figure(1)
+    plot(beta,a,'r',beta,bf,'b','linewidth',2)
+    xlabel('\beta','fontsize',16)
+    ylabel('c^*/c_e','fontsize',16)
+    text(2,.4,'standing waves','fontsize',18)
+    text(.5, .1,'traveling waves','fontsize',18)
 
-plot(beta,a,'r',beta,bf,'b','linewidth',2)
-xlabel('\beta','fontsize',16)
-ylabel('c^*/c_e','fontsize',16)
-text(2,.4,'standing waves','fontsize',18)
-text(.5, .1,'traveling waves','fontsize',18)
-
-output=[beta' a' bf'];
-
-save discrete.dat output -ascii
+%output=[beta' a' bf'];
+%save discrete.dat output -ascii
